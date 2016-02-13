76f2575da9c5edfea674da22f0aa4cbdbb89d858
donohue76 <donohue76@hotmail.com>
2016-02-12 17:08:54 -0800

    final commit

--- todolist.rb
+++ todolist.rb
@@ -27,7 +27,7 @@ class ToDoItem
       @complete = "Item complete"
     end
   end
-  # Story: As a developer, when I print a ToDoItem is done status is shown.
+  # Story: As a developer, when I print a ToDoItem,  done? status is shown.
   def done?
     if @complete == "Item complete"
       return "Item complete"
@@ -43,25 +43,67 @@ end
 # Story: As a developer, I can add all of my ToDoItems to a ToDoList.
 class ToDoList
   def initialize()
-    @array = []
+    @to_do_items = []
+    @due_items = []
+    @anniversary_items = []
   end
-  def addToArray(item)
-    @array.push(item)
+  def addToDoItems(item)
+    @to_do_items.push(item)
   end
   def toDoItems
-    return @array
+    return @to_do_items
+  end
+  def addDueItems(item)
+    @due_items.push(item)
+  end
+  def dueItems
+    return @due_items
+  end
+  def addAnniversaryItems(item)
+    @anniversary_items.push(item)
+  end
+  def anniversaryItems
+    return @anniversary_items
   end
   # Story: As a developer with a ToDoList, I can show all the completed items.
   def selectComplete
-    @array.select {|item| item.done? == 'Item complete'}
+    @to_do_items.select {|item| item.done? == 'Item complete'}
   end
   # Story: As a developer with a ToDoList, I can show all the not completed items.
   def selectIncomplete
-    @array.select {|item| item.done? == 'Item incomplete'}
+    @to_do_items.select {|item| item.done? == 'Item incomplete'}
   end
   #Story: As a developer with a ToDoList, I can list all the not completed items that are due today.
   def selectIncompleteDueDate
-    @array.select {|item| [item.dueToday? == true]}
+    @due_items.select{|item| item.dueToday? && item.done? == 'Item incomplete'}
+  end
+  #Story: As a developer with a ToDoList, I can list all the not completed items in order of due date.
+  def selectIncompleteOrderByDate
+    @due_items.sort!{|x,y| x.dueDate <=> y.dueDate}
+    @due_items.select{|item| item.done? == 'Item incomplete'}
+  end
+  # Story: As a developer with a ToDoList with and without due dates, I can show all the not completed items in order of due date, and then the items without due dates.
+  def incompleteItemsWithAndWithoutDates
+    @due_items.sort!{|x,y| x.dueDate <=> y.dueDate}
+    @due_items.select!{|item| item.done? == 'Item incomplete'}
+    @to_do_items.select!{|item| item.done? == 'Item incomplete'}
+    @incomplete_array = @due_items.concat(@to_do_items)
+  end
+  # Story: As a developer with a ToDoList with and without due dates and yearly recurring dates, I can list all the not completed items in order of due date and yearly dates for the current month.
+  def itemsOfThisMonth
+    @due_items.sort!{|x,y| x.dueDate <=> y.dueDate}
+    @due_items.select!{|item| item.done? == 'Item incomplete'}
+    @anniversary_items.select!{|item| item.month}
+    @incomplete_and_month = @due_items.concat(@anniversary_items)
+  end
+  # Story: As a developer with a ToDoList with a collection of items with and without due dates and yearly recurring dates, I can list all the not completed items in order of due date and yearly dates for the current month, then the items without due dates.
+  def allItemsIncompleteAndYearlyAndDue
+    @due_items.sort!{|x,y| x.dueDate <=> y.dueDate}
+    @due_items.select!{|item| item.done? == 'Item incomplete'}
+    @anniversary_items.select!{|item| item.month}
+    @to_do_items.select!{|item| item.done? == 'Item incomplete'}
+    @incomplete_and_month = @due_items.concat(@anniversary_items)
+    @incomplete_and_month_and_no_date = @incomplete_and_month.concat(@to_do_items)
   end
 end
 
@@ -72,20 +114,40 @@ class ItemWithDate < ToDoItem
     @dueDate = dueDate
     super(title, desc, complete)
   end
+  def dueDate
+    return @dueDate
+  end
   #lets the user change the default date
   def changeDueDate(date)
     @dueDate = Date.parse(date)
     return @dueDate
   end
   def dueToday?
-    if @dueDate == Date.today
-      return true
-    else
-      return false
-    end
+    @dueDate == Date.today
   end
   # Story: As a developer, I can print an item with a due date with field labels and values.
   def labels
     return "Title: #{@title}; Description: #{@desc}; #{@complete}, Due Date: #{@dueDate}"
+  end
+end
+
+# Story: As a developer, I can create a to do item for an anniversary (a yearly recurring event)
+class AnniversaryItem < ToDoItem
+  def initialize(title='', desc='', complete = 'Item incomplete', month='', day='')
+    super(title, desc, complete)
+    @month = month
+    @day = day
+  end
+  def anniDate(month,day)
+    @month = month.to_s
+    @day = day.to_s
+    @anni = "#{@month}/#{@day}"
+    return @anni
+  end
+  def labels
+    return "Title: #{@title}; Description: #{@desc}; #{@complete}, Anniversary: #{@anni}"
+  end
+  def month
+    @month == Date.today.month.to_s
   end
 end
--- todolist_rspec.rb
+++ todolist_rspec.rb
@@ -45,9 +45,9 @@ describe 'ToDoList' do
     newItem2 = ToDoItem.new("2","2")
     newItem3 = ToDoItem.new("3","3")
     newList = ToDoList.new
-    newList.addToArray(newItem1)
-    newList.addToArray(newItem2)
-    newList.addToArray(newItem3)
+    newList.addToDoItems(newItem1)
+    newList.addToDoItems(newItem2)
+    newList.addToDoItems(newItem3)
     expect{newList}.to_not raise_error
     expect(newList.toDoItems.length).to eq(3)
   end
@@ -61,9 +61,9 @@ describe 'ToDoList' do
     newItem1.markDone
     newItem3.markDone
     newList = ToDoList.new
-    newList.addToArray(newItem1)
-    newList.addToArray(newItem2)
-    newList.addToArray(newItem3)
+    newList.addToDoItems(newItem1)
+    newList.addToDoItems(newItem2)
+    newList.addToDoItems(newItem3)
     expect{newList}.to_not raise_error
     expect(newList.selectComplete).to eq([newItem1, newItem3])
   end
@@ -76,24 +76,135 @@ describe 'ToDoList' do
     newItem1.markDone
     newItem3.markDone
     newList = ToDoList.new
-    newList.addToArray(newItem1)
-    newList.addToArray(newItem2)
-    newList.addToArray(newItem3)
+    newList.addToDoItems(newItem1)
+    newList.addToDoItems(newItem2)
+    newList.addToDoItems(newItem3)
     expect{newList}.to_not raise_error
     expect(newList.selectIncomplete).to eq([newItem2])
   end
+  # Story: As a developer with a ToDoList, I can list all the not completed items that are due today.
   it "should select items with due dates today and that are incomplete" do
+    newList = ToDoList.new
     newItem1 = ItemWithDate.new("1","1")
     newItem2 = ItemWithDate.new("2","2")
     newItem2.changeDueDate("2016-3-20")
-    newList = ToDoList.new
-    newList.addToArray(newItem1)
-    newList.addToArray(newItem2)
+    newList.addDueItems(newItem1)
+    newList.addDueItems(newItem2)
     expect{newList}.to_not raise_error
     expect(newList.selectIncompleteDueDate).to eq([newItem1])
   end
-
-
+  # Story: As a developer with a ToDoList, I can list all the not completed items in order of due date.
+  it "should select items that are incomplete and sort by date" do
+    newList = ToDoList.new
+    newItem1 = ItemWithDate.new("1","1")
+    newItem2 = ItemWithDate.new("2","2")
+    newItem3 = ItemWithDate.new("3","3")
+    newItem4 = ItemWithDate.new("4","4")
+    newItem2.changeDueDate("2016-3-20")
+    newItem3.changeDueDate("2016-1-22")
+    newItem4.changeDueDate("2017-7-5")
+    newItem3.markDone
+    newList.addDueItems(newItem1)
+    newList.addDueItems(newItem2)
+    newList.addDueItems(newItem3)
+    newList.addDueItems(newItem4)
+    expect{newList}.to_not raise_error
+    expect(newList.selectIncompleteOrderByDate).to eq([newItem1, newItem2, newItem4])
+  end
+  # Story: As a developer with a ToDoList with and without due dates, I can show all the not completed items in order of due date, and then the items without due dates.
+  it "should select incomplete items with and without dates and sort by date, with items without date at the end" do
+    newList = ToDoList.new
+    newItem1 = ToDoItem.new("1","1")
+    newItem2 = ToDoItem.new("2","2")
+    newItem3 = ToDoItem.new("3","3")
+    newItem1.markDone
+    newItem3.markDone
+    newList.addToDoItems(newItem1)
+    newList.addToDoItems(newItem2)
+    newList.addToDoItems(newItem3)
+    newItem4 = ItemWithDate.new("4","4")
+    newItem5 = ItemWithDate.new("5","5")
+    newItem6 = ItemWithDate.new("6","6")
+    newItem7 = ItemWithDate.new("7","7")
+    newItem5.changeDueDate("2016-3-20")
+    newItem6.changeDueDate("2016-1-22")
+    newItem7.changeDueDate("2017-7-5")
+    newItem6.markDone
+    newList.addDueItems(newItem4)
+    newList.addDueItems(newItem5)
+    newList.addDueItems(newItem6)
+    newList.addDueItems(newItem7)
+    expect{newList}.to_not raise_error
+    expect(newList.incompleteItemsWithAndWithoutDates).to eq([newItem4, newItem5,newItem7,newItem2])
+  end
+  # Story: As a developer with a ToDoList with and without due dates and yearly recurring dates, I can list all the not completed items in order of due date and yearly dates for the current month.
+  it "should select incomplete items and order by date and select yearly items for the current month" do
+    newList = ToDoList.new
+    newItem1 = ItemWithDate.new("1","1")
+    newItem2 = ItemWithDate.new("2","2")
+    newItem3 = ItemWithDate.new("3","3")
+    newItem4 = ItemWithDate.new("4","4")
+    newItem2.changeDueDate("2016-3-20")
+    newItem3.changeDueDate("2016-1-22")
+    newItem4.changeDueDate("2017-7-5")
+    newItem3.markDone
+    newList.addDueItems(newItem1)
+    newList.addDueItems(newItem2)
+    newList.addDueItems(newItem3)
+    newList.addDueItems(newItem4)
+    newItem5 = AnniversaryItem.new("5","5")
+    newItem6 = AnniversaryItem.new("6","6")
+    newItem7 = AnniversaryItem.new("7","7")
+    newItem8 = AnniversaryItem.new("8","8")
+    newItem5.anniDate(2,18)
+    newItem6.anniDate(5,13)
+    newItem7.anniDate(2,1)
+    newItem8.anniDate(2,21)
+    newList.addAnniversaryItems(newItem5)
+    newList.addAnniversaryItems(newItem6)
+    newList.addAnniversaryItems(newItem7)
+    newList.addAnniversaryItems(newItem8)
+    expect{newList}.to_not raise_error
+    expect(newList.itemsOfThisMonth).to eq([newItem1, newItem2, newItem4, newItem5,newItem7, newItem8])
+  end
+  # Story: As a developer with a ToDoList with a collection of items with and without due dates and yearly recurring dates, I can list all the not completed items in order of due date and yearly dates for the current month, then the items without due dates.
+  it "should select items and order by date, then current year, then items without date" do
+    newList = ToDoList.new
+    newItem1 = ItemWithDate.new("1","1")
+    newItem2 = ItemWithDate.new("2","2")
+    newItem3 = ItemWithDate.new("3","3")
+    newItem4 = ItemWithDate.new("4","4")
+    newItem2.changeDueDate("2016-3-20")
+    newItem3.changeDueDate("2016-1-22")
+    newItem4.changeDueDate("2017-7-5")
+    newItem3.markDone
+    newList.addDueItems(newItem1)
+    newList.addDueItems(newItem2)
+    newList.addDueItems(newItem3)
+    newList.addDueItems(newItem4)
+    newItem5 = AnniversaryItem.new("5","5")
+    newItem6 = AnniversaryItem.new("6","6")
+    newItem7 = AnniversaryItem.new("7","7")
+    newItem8 = AnniversaryItem.new("8","8")
+    newItem5.anniDate(2,18)
+    newItem6.anniDate(5,13)
+    newItem7.anniDate(2,1)
+    newItem8.anniDate(2,21)
+    newList.addAnniversaryItems(newItem5)
+    newList.addAnniversaryItems(newItem6)
+    newList.addAnniversaryItems(newItem7)
+    newList.addAnniversaryItems(newItem8)
+    newItem9 = ToDoItem.new("9","9")
+    newItem10 = ToDoItem.new("10","10")
+    newItem11 = ToDoItem.new("11","11")
+    newItem9.markDone
+    newItem11.markDone
+    newList.addToDoItems(newItem9)
+    newList.addToDoItems(newItem10)
+    newList.addToDoItems(newItem11)
+    expect{newList}.to_not raise_error
+    expect(newList.allItemsIncompleteAndYearlyAndDue).to eq([newItem1, newItem2, newItem4, newItem5, newItem7, newItem8, newItem10])
+  end
 end
 
 # Story: As a developer, I can create a to do item with a due date, which can be changed.
@@ -109,6 +220,26 @@ describe ItemWithDate do
 
   it "should print out the field labels and values" do
     newItem = ItemWithDate.new
-    expect(newItem.labels).to eq("Title: ; Description: ; Item incomplete, Due Date: 2016-02-11")
+    newItem.changeDueDate("2016-02-12")
+    expect(newItem.labels).to eq("Title: ; Description: ; Item incomplete, Due Date: 2016-02-12")
+  end
+end
+describe AnniversaryItem do
+  it "should create a new item" do
+    expect{AnniversaryItem.new}.to_not raise_error
+  end
+  it "should add a date to the item" do
+    anniItem = AnniversaryItem.new
+    expect(anniItem.anniDate(3,13)).to eq("3/13")
+  end
+  it "should return labels" do
+    anniItem = AnniversaryItem.new
+    expect(anniItem.anniDate(3,13)).to eq("3/13")
+    expect(anniItem.labels).to eq("Title: ; Description: ; Item incomplete, Anniversary: 3/13")
+  end
+  it "should return the month" do
+    anniItem = AnniversaryItem.new
+    expect(anniItem.anniDate(2,13)).to eq("2/13")
+    expect(anniItem.month).to eq(true)
   end
 end
